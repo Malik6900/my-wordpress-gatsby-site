f09812ffe2024000bc9c3a4f63c43296d0b1d746
malik6900 <alikarriem786@gmail.com>
2022-11-27 22:25:45 +0200

    Add comma gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
           },
         },
       ],
-    }
+    },
 
     // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
     //`gatsby-plugin-react-helmet`,
